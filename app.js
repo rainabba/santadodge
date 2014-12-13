7fec680dcf72e9d2d8a3db8a90162d76905baaee
John Absalon <drahgon55@gmail.com>
2014-12-13 14:28:31 -0700

    added express changed three.html to index.html

--- app.js
+++ app.js
@@ -1,48 +1,20 @@
-var app = require('http').createServer(handler);
+var express = require('express')
+var app = express()
+var socketio = require('socket.io')
+
+var http = require('http')
 var url= require('url');
 var fs = require('fs');
 var os = require('os');
-var io = require('socket.io').listen(app);
+app.use(express.static(__dirname))
 
-app.listen(5000);
+var server = http.createServer(app)
+io = socketio.listen(server)
+server.listen(5000)
+io.sockets.on('connection',connectionCB)
 
 
-// Http handler function
-function handler (req, res) {
-    
-    // Using URL to parse the requested URL
-    var path = url.parse(req.url).pathname;
-    
-    // Managing the root route
-    if (path == '/') {
-        index = fs.readFile(__dirname+'/three.html', 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load three.html");
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(data);
-            });
-
-    // Managing the route for the javascript files
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname+path, 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
-    
+function connectionCB (socket) {
+	
+	
 }
